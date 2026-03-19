04b4c3df76967e20536d0edbd9b7792e72199ffd
moralpriest <moralpriest@tutamail.com>
2026-03-19 11:36:08 -0300

    fix: cherry-pick race conditions, panic recovery, and buffered channel
    - atomic.Bool for Connected, Closing, Writing, migrating fields
    - atomic.Value for global Connected variable
    - defer/recover in all goroutines
    - Make Exit_In_Progress channel buffered (1) to prevent deadlock

--- cmd/gnomonindexer/gnomonindexer.go
+++ cmd/gnomonindexer/gnomonindexer.go
@@ -12,6 +12,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/chzyer/readline"
@@ -33,7 +34,7 @@ type GnomonServer struct {
 	LastIndexedHeight int64
 	SearchFilters     []string
 	Indexers          map[string]*indexer.Indexer
-	Closing           bool
+	Closing           atomic.Bool
 	DaemonEndpoint    string
 	RunMode           string
 	DBType            string
@@ -77,7 +78,7 @@ Options:
   --derodb-dir=<"">     Defines the location where derodb can be referenced from
   --debug     Enables debug logging`
 
-var Exit_In_Progress = make(chan bool)
+var Exit_In_Progress = make(chan bool, 1)
 
 var RLI *readline.Instance
 
@@ -405,6 +406,11 @@ func main() {
 	Gnomon.Indexers[csearch_filter] = defaultIndexer
 
 	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				logger.Errorf("[readline_loop] PANIC recovered: %v", r)
+			}
+		}()
 		for {
 			if err = Gnomon.readline_loop(RLI); err == nil {
 				break
@@ -414,6 +420,11 @@ func main() {
 
 	// This tiny goroutine continuously updates status as required
 	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				logger.Errorf("[status_update] PANIC recovered: %v", r)
+			}
+		}()
 		for {
 			select {
 			case <-Exit_In_Progress:
@@ -421,7 +432,7 @@ func main() {
 				return
 			default:
 			}
-			if Gnomon.Closing {
+			if Gnomon.Closing.Load() {
 				return
 			}
 
@@ -1764,7 +1775,7 @@ func scidExist(s []string, str string) bool {
 }
 
 func (g *GnomonServer) Close() {
-	g.Closing = true
+	g.Closing.Store(true)
 
 	for _, v := range g.Indexers {
 		go v.Close()
